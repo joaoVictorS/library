5545b362bea7a8e933660609d83f93336557a077
joaoVictorS <jonnymcz2@gmail.com>
2023-05-14 21:12:06 -0300

    crud testamento

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TestamentoController;
+use App\Http\Controllers\LivroController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/testamento', [TestamentoController::class, 'index']);
+Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
+Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
+Route::post('/testamento', [TestamentoController::class, 'store']);
+Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
+
+Route::get('/livro', [LivroController::class, 'index']);
+Route::get('/livro/{id}', [LivroController::class, 'show']);
+Route::put('/livro/{id}', [LivroController::class, 'update']);
+Route::post('/livro', [LivroController::class, 'store']);
+Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
